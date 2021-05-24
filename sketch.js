95126bf6a7f87abf18cfa5d0857a9da3bbef3b56
Satvik-art-creator <67773620+Satvik-art-creator@users.noreply.github.com>
2021-05-24 14:52:08 +0530

    hjhh

--- sketch.js
+++ sketch.js
@@ -38,7 +38,7 @@ var thunder, thunderImage1, thunderImage2, thunderImage3, thunderImage4;
 var star = [];
 var starImage;
 
-var score = 2000;
+var score = 3000;
 
 localStorage["HighestScore"] = 0;
 
