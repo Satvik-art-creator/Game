738f91042c0616fe2889c70bab6f222af46973c1
Satvik-art-creator <67773620+Satvik-art-creator@users.noreply.github.com>
2021-05-30 11:50:48 +0530

    sattu

--- sketch.js
+++ sketch.js
@@ -270,7 +270,7 @@ function draw() {
         robin.changeAnimation("running", robin_running);
       }
     
-    $(window).on('swipedown', function() {
+    $(window).bind("swipe.down", function() {
       if(frameCount % 50 != 0){
         robin.changeAnimation("slide", robin_slide);
 //         sliding.play();
